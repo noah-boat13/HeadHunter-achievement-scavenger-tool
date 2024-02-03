9c6b20129b7b466f7651622471beb1a8184856c7
Tim Lewis <timothy.james.lewis.31@gmail.com>
2024-02-02 21:20:49 -0600

    added youtube link

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,15 @@ const rawgKey = "3aa9c76d2f81440cb15bd3f113bf0db5"
 
 var ytURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=skyrim%20how%20to%20get%20dragon%20rider%20achievement&key=AIzaSyDLxdkAoPEq_7O3GIEVsz7vhGPmt1ffXtY"
 
-fetch("https://api.rawg.io/api/games?key=3aa9c76d2f81440cb15bd3f113bf0db5&search=skyrim").then(res => res.json()).then(data=> {
-    console.log(data);
-})
+// fetch("https://api.rawg.io/api/games?key=3aa9c76d2f81440cb15bd3f113bf0db5&search=skyrim").then(res => res.json()).then(data=> {
+//     console.log(data);
+// })
 
-fetch("https://api.rawg.io/api/games/5679/achievements?key=3aa9c76d2f81440cb15bd3f113bf0db5&page=1&page_size=40").then(res => res.json()).then(data=> {
+// fetch("https://api.rawg.io/api/games/5679/achievements?key=3aa9c76d2f81440cb15bd3f113bf0db5&page=1&page_size=40").then(res => res.json()).then(data=> {
+//     console.log(data);
+// })
+
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=skyrim%20how%20to%20get%20dragon%20rider%20achievement&key=AIzaSyDLxdkAoPEq_7O3GIEVsz7vhGPmt1ffXtY").then(res => res.json()).then(data=> {
     console.log(data);
 })
 
