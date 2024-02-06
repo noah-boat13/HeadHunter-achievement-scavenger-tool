6c558b8980cb3069601a45a788c0442d11e2404c
Tim Lewis <timothy.james.lewis.31@gmail.com>
2024-02-05 22:15:27 -0600

    added functionality so the search button retrieves the data of whatever is written in the search input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,32 @@
+var gameSearchInput = document.querySelector("#game-search");
+var searchBtn = document.querySelector("#search-button");
 // our youtube key
-const ytKey = "AIzaSyDLxdkAoPEq_7O3GIEVsz7vhGPmt1ffXtY"
+const ytKey = "AIzaSyDLxdkAoPEq_7O3GIEVsz7vhGPmt1ffXtY";
 
 // our RAWG key
-const rawgKey = "3aa9c76d2f81440cb15bd3f113bf0db5"
+const rawgKey = "3aa9c76d2f81440cb15bd3f113bf0db5";
 
-var ytURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=skyrim%20how%20to%20get%20dragon%20rider%20achievement&key=AIzaSyDLxdkAoPEq_7O3GIEVsz7vhGPmt1ffXtY"
+// empty holder for game search. value is changed at the searchBtn event listener
+var gameSearchKeyword = "";
+
+var ytURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=skyrim%20how%20to%20get%20dragon%20rider%20achievement&key=AIzaSyDLxdkAoPEq_7O3GIEVsz7vhGPmt1ffXtY";
 
 // this is where we search our game
 function searchGame(){
-fetch("https://api.rawg.io/api/games?key=3aa9c76d2f81440cb15bd3f113bf0db5&search=skyrim").then(res => res.json()).then(data=> {
+fetch("https://api.rawg.io/api/games?key=3aa9c76d2f81440cb15bd3f113bf0db5&search=" + gameSearchKeyword).then(res => res.json()).then(data=> {
     console.log(data);
 })
 }
+
+// when the search button is clicked it updates the value to enter into the url to search the game
+searchBtn.addEventListener("click", function(event){
+    event.preventDefault();
+
+    gameSearchKeyword = gameSearchInput.value.trim()
+    console.log (gameSearchKeyword)
+    
+    searchGame();
+})
 
 // get achievments from searched Game
 function getAchievments(){
